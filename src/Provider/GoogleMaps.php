0650185824883221fe42a2d677d3ffce29e99432
istvan <istvan@neogen.biz>
2017-02-23 19:35:20 +0200

    Revent GoogleMaps - compat with GeocoderBundle

--- src/Provider/GoogleMaps.php
+++ src/Provider/GoogleMaps.php
@@ -12,27 +12,25 @@ namespace Geocoder\Provider;
 
 use Exception;
 use Geocoder\Exception\InvalidCredentials;
-use Geocoder\Exception\InvalidServerResponse;
 use Geocoder\Exception\NoResult;
 use Geocoder\Exception\QuotaExceeded;
 use Geocoder\Exception\UnsupportedOperation;
-use Geocoder\Exception\ZeroResults;
-use Http\Client\HttpClient;
+use Ivory\HttpAdapter\HttpAdapterInterface;
 
 /**
  * @author William Durand <william.durand1@gmail.com>
  */
-final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvider
+class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvider
 {
     /**
      * @var string
      */
-    const GEOCODE_ENDPOINT_URL_SSL = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s';
+    const ENDPOINT_URL = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s';
 
     /**
      * @var string
      */
-    const REVERSE_ENDPOINT_URL_SSL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=%F,%F';
+    const ENDPOINT_URL_SSL = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s';
 
     use LocaleTrait;
 
@@ -42,53 +40,29 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
     private $region;
 
     /**
+     * @var bool
+     */
+    private $useSsl;
+
+    /**
      * @var string
      */
     private $apiKey;
 
     /**
-     * @var string
+     * @param HttpAdapterInterface $adapter An HTTP adapter
+     * @param string               $locale  A locale (optional)
+     * @param string               $region  Region biasing (optional)
+     * @param bool                 $useSsl  Whether to use an SSL connection (optional)
+     * @param string               $apiKey  Google Geocoding API key (optional)
      */
-    private $clientId;
-
-    /**
-     * @var string
-     */
-    private $privateKey;
-
-    /**
-     * Google Maps for Business
-     * https://developers.google.com/maps/documentation/business/
-     *
-     * @param HttpClient $client An HTTP adapter
-     * @param string     $clientId Your Client ID
-     * @param string     $privateKey Your Private Key (optional)
-     * @param string     $locale A locale (optional)
-     * @param string     $region Region biasing (optional)
-     * @param string     $apiKey Google Geocoding API key (optional)
-     * @return GoogleMaps
-     */
-    public static function business(HttpClient $client, $clientId, $privateKey = null, $locale = null, $region = null, $apiKey = null)
+    public function __construct(HttpAdapterInterface $adapter, $locale = null, $region = null, $useSsl = false, $apiKey = null)
     {
-        $provider = new self($client, $locale, $region, $apiKey);
-        $provider->clientId = $clientId;
-        $provider->privateKey = $privateKey;
-
-        return $provider;
-    }
-
-    /**
-     * @param HttpClient $client An HTTP adapter
-     * @param string     $locale A locale (optional)
-     * @param string     $region Region biasing (optional)
-     * @param string     $apiKey Google Geocoding API key (optional)
-     */
-    public function __construct(HttpClient $client, $locale = null, $region = null, $apiKey = null)
-    {
-        parent::__construct($client);
+        parent::__construct($adapter);
 
         $this->locale = $locale;
         $this->region = $region;
+        $this->useSsl = $useSsl;
         $this->apiKey = $apiKey;
     }
 
@@ -103,7 +77,10 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
             throw new UnsupportedOperation('The GoogleMaps provider does not support IP addresses, only street addresses.');
         }
 
-        $query = sprintf(self::GEOCODE_ENDPOINT_URL_SSL, rawurlencode($address));
+        $query = sprintf(
+            $this->useSsl ? self::ENDPOINT_URL_SSL : self::ENDPOINT_URL,
+            rawurlencode($address)
+        );
 
         return $this->executeQuery($query);
     }
@@ -113,9 +90,7 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
      */
     public function reverse($latitude, $longitude)
     {
-        $query = sprintf(self::REVERSE_ENDPOINT_URL_SSL, $latitude, $longitude);
-
-        return $this->executeQuery($query);
+        return $this->geocode(sprintf('%F,%F', $latitude, $longitude));
     }
 
     /**
@@ -138,7 +113,7 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
      *
      * @return string query with extra params
      */
-    private function buildQuery($query)
+    protected function buildQuery($query)
     {
         if (null !== $this->getLocale()) {
             $query = sprintf('%s&language=%s', $query, $this->getLocale());
@@ -152,14 +127,6 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
             $query = sprintf('%s&key=%s', $query, $this->apiKey);
         }
 
-        if (null !== $this->clientId) {
-            $query = sprintf('%s&client=%s', $query, $this->clientId);
-
-            if (null !== $this->privateKey) {
-                $query = $this->signQuery($query);
-            }
-        }
-
         return $query;
     }
 
@@ -169,8 +136,7 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
     private function executeQuery($query)
     {
         $query   = $this->buildQuery($query);
-        $request = $this->getMessageFactory()->createRequest('GET', $query);
-        $content = (string) $this->getHttpClient()->sendRequest($request)->getBody();
+        $content = (string) $this->getAdapter()->get($query)->getBody();
 
         // Throw exception if invalid clientID and/or privateKey used with GoogleMapsBusinessProvider
         if (strpos($content, "Provided 'signature' is not valid for the provided client ID") !== false) {
@@ -178,14 +144,14 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
         }
 
         if (empty($content)) {
-            throw InvalidServerResponse::create($query);
+            throw new NoResult(sprintf('Could not execute query "%s".', $query));
         }
 
         $json = json_decode($content);
 
         // API error
         if (!isset($json)) {
-            throw InvalidServerResponse::create($query);
+            throw new NoResult(sprintf('Could not execute query "%s".', $query));
         }
 
         if ('REQUEST_DENIED' === $json->status && 'The provided API key is invalid.' === $json->error_message) {
@@ -204,7 +170,7 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
 
         // no result
         if (!isset($json->results) || !count($json->results) || 'OK' !== $json->status) {
-            throw ZeroResults::create($query);
+            throw new NoResult(sprintf('Could not execute query "%s".', $query));
         }
 
         $results = [];
@@ -264,7 +230,6 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
                 break;
 
             case 'locality':
-            case 'postal_town':
                 $resultSet['locality'] = $values->long_name;
                 break;
 
@@ -301,32 +266,5 @@ final class GoogleMaps extends AbstractHttpProvider implements LocaleAwareProvid
         }
 
         return $resultSet;
-    }
-    
-    /**
-     * Sign a URL with a given crypto key
-     * Note that this URL must be properly URL-encoded
-     * src: http://gmaps-samples.googlecode.com/svn/trunk/urlsigning/UrlSigner.php-source
-     *
-     * @param string $query Query to be signed
-     *
-     * @return string $query Query with signature appended.
-     */
-    private function signQuery($query)
-    {
-        $url = parse_url($query);
-
-        $urlPartToSign = $url['path'] . '?' . $url['query'];
-
-        // Decode the private key into its binary format
-        $decodedKey = base64_decode(str_replace(array('-', '_'), array('+', '/'), $this->privateKey));
-
-        // Create a signature using the private key and the URL-encoded
-        // string using HMAC SHA1. This signature will be binary.
-        $signature = hash_hmac('sha1', $urlPartToSign, $decodedKey, true);
-
-        $encodedSignature = str_replace(array('+', '/'), array('-', '_'), base64_encode($signature));
-
-        return sprintf('%s&signature=%s', $query, $encodedSignature);
     }
 }
